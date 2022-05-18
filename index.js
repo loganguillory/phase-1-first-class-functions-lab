31cce452bd1809e22678bdfc6c142a4dcc0211f0
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-05-18 11:17:48 -0400

    commit tree de81a8b46d081e759bae6a00e2b6e888debc9722

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers){
+    return arrayOfDrivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(arrayOfDrivers){
+    return arrayOfDrivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(i){
+    return function(fareMultiplier){
+        return fareMultiplier * i
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)    
+
+const selectDifferentDrivers = function(arrayOfDrivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
